8fed0813e7dc61f0848f1cf5cf263ea34a200e8b
Pierre Lambert <supagoof924@hotmail.com>
2019-05-23 16:15:10 +0200

    [ FEATURE ] Updated OAuth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,37 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+
+  state = {
+    isSignedIn : null
+  }
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "992071234658-re7hqd97kellcsuudl1lsqnj7ajtci1o.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "992071234658-re7hqd97kellcsuudl1lsqnj7ajtci1o.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({
+            isSignedIn: this.auth.isSignedIn.get()
+          });
+        });
     });
   }
 
+renderAuthButton(){
+if (this.state.isSignedIn === null) {
+  return <div></div>
+} else if (this.state.isSignedIn) return <div>Logged in</div>
+else return <div>Log in</div>
+}
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
