a2efeafac8321ab0533824c845d3214f9eec96ab
Pierre Lambert <supagoof924@hotmail.com>
2019-05-23 15:31:50 +0200

    [ FEATURE ] Added OAuth

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "992071234658-re7hqd97kellcsuudl1lsqnj7ajtci1o.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
