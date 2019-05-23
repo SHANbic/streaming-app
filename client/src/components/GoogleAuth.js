aca39ea5da5959ec0479fc0c7e00a88c22d66962
Pierre Lambert <supagoof924@hotmail.com>
2019-05-23 16:30:00 +0200

    [ FEATURE ] Updated OAuth to be live rendered

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
-
   state = {
-    isSignedIn : null
-  }
+    isSignedIn: null
+  };
 
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
@@ -19,16 +18,21 @@ class GoogleAuth extends React.Component {
           this.setState({
             isSignedIn: this.auth.isSignedIn.get()
           });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
-renderAuthButton(){
-if (this.state.isSignedIn === null) {
-  return <div></div>
-} else if (this.state.isSignedIn) return <div>Logged in</div>
-else return <div>Log in</div>
-}
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div />;
+    } else if (this.state.isSignedIn) return <div>Logged in</div>;
+    else return <div>Log in</div>;
+  }
 
   render() {
     return <div>{this.renderAuthButton()}</div>;
