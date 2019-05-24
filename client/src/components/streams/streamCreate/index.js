04814a9a83992a09519a9b890d6f796fe054be3d
Pierre Lambert <supagoof924@hotmail.com>
2019-05-24 17:52:44 +0200

    [ FEATURE ] Added labels to form

--- client/src/components/streams/streamCreate/index.js
+++ client/src/components/streams/streamCreate/index.js
@@ -2,15 +2,28 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Enter a title for your stream"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description"
+        />
       </form>
     );
   }
