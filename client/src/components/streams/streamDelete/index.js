28c7afd94eb7a14c6f359b4109344b45810ef289
Pierre Lambert <supagoof924@hotmail.com>
2019-05-27 22:31:15 +0200

    [ FEATURE ] Turned StreamDelete to class component to handle lifecycle methods and connected it to redux store

--- client/src/components/streams/streamDelete/index.js
+++ client/src/components/streams/streamDelete/index.js
@@ -1,26 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../../actions";
 import Modal from "../../Modal";
 import history from "../../../history";
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui negative button">DELETE</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      Stream Delete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream? ( this can't be undone )"
-        actions={actions}
-        onDismiss={() => history.push("/")}
-      />
-    </div>
-  );
-};
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui negative button">DELETE</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+  render() {
+    return (
+      <div>
+        Stream Delete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream? ( this can't be undone )"
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
 
-export default StreamDelete;
+/* const mapStateToProps = state => {
+  return null;
+}; */
+export default connect(
+  null,
+  { fetchStream }
+)(StreamDelete);
