bfab18e6821df5cc24b73e726c38d8b59b911a6c
Pierre Lambert <supagoof924@hotmail.com>
2019-05-27 18:33:36 +0200

    [ FEATURE ] Updated StreamDelete to pass props to Modal as a matter of reusability

--- client/src/components/streams/streamDelete/index.js
+++ client/src/components/streams/streamDelete/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "../../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui negative button">DELETE</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       Stream Delete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream? ( this can't be undone )"
+        actions={actions}
+      />
     </div>
   );
 };
