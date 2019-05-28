2c4fb96bdcab9b32a5d969ce3054160f71e031c5
Pierre Lambert <supagoof924@hotmail.com>
2019-05-28 12:33:16 +0200

    [ FEATURE ] Added a video tag into StreamShow component

--- client/src/components/streams/streamShow/index.js
+++ client/src/components/streams/streamShow/index.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
   render() {
-    if (!this.props.stream) {
-      return "";
-    }
+    if (!this.props.stream) return "";
+
     const { title, description } = this.props.stream;
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
